9e6f99891437a981ca8a8ba0970ce1afd3444795
Khushi Arora <khushiarora752002@gmail.com>
2022-08-02 13:23:49 +0530

    login

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,7 +1,25 @@
 //already registered
 import Bar from '../../Components/Navbar/Bar';
+import React,{useState} from 'react';
+import axios from 'axios';
 
 function Login() {
+    const [useremail, setEmail] = useState();
+    const [password, setPass] = useState();
+    const handleSignin = async () => {
+        const user = {
+            useremail,
+            password,
+          };
+          await axios.post("http://localhost:4000/api/login", user).then(function (response) {
+          if(response.data.token)
+          {
+            localStorage.setItem("token",response.data.token);
+          }
+      }).catch(function(error){
+        console.log(error);
+      })
+    }
   return (
     <div>
     <Bar />
@@ -20,15 +38,19 @@ function Login() {
       </span>
     </div>
     <div className="flex justify-center my-2 mx-4 md:mx-0">
-    <form className="w-full max-w-xl bg-white rounded-lg shadow-md p-6">
+    <div className="w-full max-w-xl bg-white rounded-lg shadow-md p-6">
         <div className="flex flex-wrap -mx-3 mb-6">
             <div className="w-full md:w-full px-3 mb-6">
                 <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for='Password'>Email address</label>
-                <input className="appearance-none block w-full bg-white text-gray-900 font-medium border border-gray-400 rounded-lg py-3 px-3 leading-tight focus:outline-none" type='email'  required />
+                <input onChange={(e) =>{
+                    setEmail(e.target.value);
+                }} className="appearance-none block w-full bg-white text-gray-900 font-medium border border-gray-400 rounded-lg py-3 px-3 leading-tight focus:outline-none" type='email'  required />
             </div>
             <div className="w-full md:w-full px-3 mb-6">
                 <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for='Password'>Password</label>
-                <input className="appearance-none block w-full bg-white text-gray-900 font-medium border border-gray-400 rounded-lg py-3 px-3 leading-tight focus:outline-none" type='password' required />
+                <input onChange={(e) =>{
+                    setPass(e.target.value);
+                }} className="appearance-none block w-full bg-white text-gray-900 font-medium border border-gray-400 rounded-lg py-3 px-3 leading-tight focus:outline-none" type='password' required />
             </div>
             <div className="w-full flex items-center justify-between px-3 mb-3 ">
                 <label for="remember" className="flex items-center w-1/2">
@@ -40,7 +62,9 @@ function Login() {
                 </div>
             </div>
             <div className="w-full md:w-full px-3 mb-6">
-                <button className="appearance-none block w-full bg-blue-600 text-gray-100 font-bold border border-gray-200 rounded-lg py-3 px-3 leading-tight hover:bg-blue-500 focus:outline-none focus:bg-white focus:border-gray-500">Sign in</button>
+                <button onClick={ ()=>{
+                    handleSignin();
+                }} className="appearance-none block w-full bg-blue-600 text-gray-100 font-bold border border-gray-200 rounded-lg py-3 px-3 leading-tight hover:bg-blue-500 focus:outline-none focus:bg-white focus:border-gray-500">Sign in</button>
             </div>
             <div className="mx-auto -mb-6 pb-1">
                 <span className="text-center text-xs text-gray-700">or sign up with</span>
@@ -69,7 +93,7 @@ function Login() {
                 </div>
             </div>
         </div>
-    </form>
+    </div>
     </div>
     </div>
 );
